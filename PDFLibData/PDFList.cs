e8baea2393d0f8df6cec41ee3d8dc45d733f2bed
MaheuTaroo <pantupa@gmail.com>
2021-08-28 23:18:39 +0100

    Half-hearted excuse of a commit: semi-adding PDF iteration

--- PDFLibData/PDFList.cs
+++ PDFLibData/PDFList.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PDFLibData
 {
     public static class PDFList
     {
         public static string[] Files;
-
         public static IEnumerable<string> GetFiles()
         {
             if (Files == null || Files.Length == 0) yield return "No files found";
             else for (int i = 0; i == Files.Length; i++) yield return Files[i];
+        }
+        public static void SaveList(params string[] Files)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                if (sfd.ShowDialog() != DialogResult.Cancel)
+                {
+
+                }
+            }
         }
     }
 }
